3a90c3346510bf7700464c20cdb8ad6d9443c693
vidigalbz <leonardovidigal00@gmail.com>
2024-08-22 17:25:18 -0300

    começando a remover o item

--- supermercado.py
+++ supermercado.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from datetime import datetime
 import tkinter.messagebox as tkmsg #Importa o tkinter.messagebox como tkmsg
 import json as j
 import os
@@ -10,7 +11,7 @@ estoque = {}
 def clear():
     for i in root.winfo_children():
         i.destroy()
-
+        
 def carregar_usuarios():
     global usuarios
     if os.path.exists("supermercado_usuarios.json"):
@@ -33,9 +34,9 @@ def carregar_estoque():
                                                         estoque[i]["Categoria"], 
                                                         estoque[i]["Lote"], 
                                                         estoque[i]["Quantidade"], 
-                                                        f"R$ {estoque[i]["Preço do Lote"]}", 
-                                                        f"R$ {estoque[i]["Preço do Unitario"]}", 
-                                                        f"R$ {estoque[i]["Preço de Venda"]}"))
+                                                        f"R$ {estoque[i]['Preço do Lote']}", 
+                                                        f"R$ {estoque[i]['Preço do Unitario']}", 
+                                                        f"R$ {estoque[i]['Preço de Venda']}"))
         except j.JSONDecodeError:
             estoque = {}
     else:
@@ -63,11 +64,17 @@ def verificar_login():
     else:
         tkmsg.showerror('login', 'credenciais inválidas')
 
-def remocao_de_produtos():       #Função para remover produtos estragados do estoque
-    teste = Label(root, text="TELA DE REMOÇÃO", font="Arial 20")
+def remocao_de_produtos():
+        selected_item = tree_estoque.selection()
+        print(selected_item)        
+    
+        # Verifica se algum item foi selecionado
+
+def compra_de_produtos():       #Função para realizar a compra de produtos novos
+    teste = Label(root, text="TELA DE COMPRAS", font="Arial 20")
     teste.place(x=410, y=10)
  
-    label_campo1 = Label(root, text='Informe aqui o ID do produto que deseja REMOVER:')
+    label_campo1 = Label(root, text='Informe aqui o ID do produto que deseja comprar:')
     label_campo1.place(x=10, y=40)
     entry_campo1 = Entry(root, width=30)
     entry_campo1.place(x=10, y = 65)
@@ -78,9 +85,8 @@ def remocao_de_produtos():       #Função para remover produtos estragados do e
     butaoselecao2 = Radiobutton(root, text="UNIDADE")
     butaoselecao2.place(x=10, y=115)
  
-    button_cancel2 = Button(root, text='Cancelar', width=20, command=lambda: [clear(), tab_estoque()])
+    button_cancel2 = Button(root, text='Cancelar', width=20, command=lambda: [clear(), tab_estoque(), carregar_estoque()])
     button_cancel2.pack(side=RIGHT, padx=20, pady=20)
-
 
 def tab_cadastro():
    global entry_username2, entry_password2, entry_password3
@@ -111,7 +117,8 @@ def tab_cadastro():
    button_cancel2.pack(side=RIGHT, padx=20, pady=20)
 
 def tab_login():
-    global login, entry_password, entry_username 
+    global login, entry_password, entry_username
+    
     login = Toplevel(root)
     login.title('Tela de Login')
     login.geometry('375x270')
@@ -143,100 +150,132 @@ def tab_login():
     button_createAccount.pack(side=RIGHT, padx=20)
 
 def cadastrar_produto():
-    if "" in (entry_nome_prod.get(), entry_categoria.get(), entry_lote.get(), entry_prclote.get(), entry_prcuni.get(), entry_prcvenda.get()):
+    data_str = entry_validade.get()
+   
+    try:
+        data_converçao = datetime.strptime(data_str, "%d/%m/%Y")
+        if data_converçao < datetime.now():
+            tkmsg.showerror("ERRO", "Este produto pode ja estar vencido!")
+            return
+    
+    except ValueError:
+            tkmsg.showerror("ERRO", "Formato de data inválido. Por favor, use o formato dd/mm/yyyy.")
+            return
+    if "" in (entry_nome_prod.get(), entry_categoria.get(), entry_validade.get(), entry_prclote.get(), entry_prcuni.get(), entry_prcvenda.get()):
         tkmsg.showerror("ERRO", "Preencha todos os campos")
-    
-    elif entry_id_prod.get().isalpha():
+        return
+    if entry_id_prod.get().isalpha():
             tkmsg.showerror("ERRO", "Insira apenas números no ID")
-    
-    elif entry_id_prod.get() in estoque:
+            return
+    if entry_id_prod.get() in estoque:
         tkmsg.showerror("ERRO", "Este ID ja existe")
+        return
+    if entry_qtd.get().isalpha():
+        tkmsg.showerror("ERRO", "Insira apenas números na quantidade")
+        return
+    if entry_prclote.get().isalpha():
+            tkmsg.showerror("ERRO", "Insira apenas números no preço do lote")
+            return
+    if entry_prcuni.get().isalpha():
+            tkmsg.showerror("ERRO", "Insira apenas números no preço do unitário")
+            return
+    if entry_prcvenda.get().isalpha():
+            tkmsg.showerror("ERRO", "Insira apenas números no preço de venda")
+            return
+    if entry_validade.get().isalpha():
+        tkmsg.showerror("ERRO", "Insira apenas números como validade")    
+        return
     
-    else:
-        estoque[entry_id_prod.get()] = {"Nome do Produto": entry_nome_prod.get(), 
+    estoque[entry_id_prod.get()] = {"Nome do Produto": entry_nome_prod.get(), 
                                         "Categoria": entry_categoria.get(), 
-                                        "Lote": entry_lote.get(), 
+                                        "Lote": entry_validade.get(), 
                                         "Quantidade": entry_qtd.get(), 
                                         "Preço do Lote": entry_prclote.get(), 
                                         "Preço do Unitario": entry_prcuni.get(),
                                         "Preço de Venda": entry_prcvenda.get()}
         
-        tree_estoque.insert("", "end", values=(entry_id_prod.get(), 
+    tree_estoque.insert("", "end", values=(entry_id_prod.get(), 
                                             entry_nome_prod.get(), 
                                             entry_categoria.get(), 
-                                            entry_lote.get(), entry_qtd.get(), f"R$ {entry_prclote.get()}", f"R$ {entry_prcuni.get()}", f"R$ {entry_prcvenda.get()}"))
+                                            entry_validade.get(), entry_qtd.get(), f"R$ {entry_prclote.get()}", f"R$ {entry_prcuni.get()}", f"R$ {entry_prcvenda.get()}"))
     
-        with open("supermercado_estoque.json" , "w", encoding="utf-8") as file:
+    with open("supermercado_estoque.json" , "w", encoding="utf-8") as file:
                 j.dump(estoque, file, indent=2)
 
     
 
 def tab_estoque():
-    global entry_id_prod, entry_nome_prod, entry_categoria, entry_lote, entry_qtd, entry_prclote, entry_prcuni, entry_prcvenda, tree_estoque
+    global entry_id_prod, entry_nome_prod, entry_categoria, entry_validade, entry_qtd, entry_prclote, entry_prcuni, entry_prcvenda, tree_estoque
     
+    label_id_prod= Label(root, text="ID")
+    label_id_prod.place(x=10, y=0)
+    entry_id_prod = Entry(root, width=30)
+    entry_id_prod.place(x=15, y=25)
+
     label_nome_prod= Label(root, text="Nome do Produto")
-    label_nome_prod.place(x=10, y=25)
+    label_nome_prod.place(x=10, y=50)
     entry_nome_prod = Entry(root, width=30)
-    entry_nome_prod.place(x=15, y=50)
+    entry_nome_prod.place(x=15, y=75)
 
     label_categoria = Label(root, text="Categoria:")
-    label_categoria.place(x=10, y=75)
+    label_categoria.place(x=10, y=100)
     entry_categoria = Entry(root, width=30)
-    entry_categoria.place(x=15, y=100)
+    entry_categoria.place(x=15, y=125)
 
-    label_lote = Label(root, text="Lote/Validade:")
-    label_lote.place(x=10, y=125)
-    entry_lote = Entry(root, width=30)
-    entry_lote.place(x=15, y=150)
+    label_validade = Label(root, text="Validade:")
+    label_validade.place(x=10, y=150)
+    entry_validade = Entry(root, width=30)
+    entry_validade.place(x=15, y=175)
 
     label_qtd = Label(root, text="Quantidade:")
-    label_qtd.place(x=10, y=175)
+    label_qtd.place(x=10, y=200)
     entry_qtd = Entry(root, width=30)
-    entry_qtd.place(x=15, y=200)
+    entry_qtd.place(x=15, y=225)
 
     label_prclote = Label(root, text="Preço do LOTE")
-    label_prclote.place(x=10, y=225)
+    label_prclote.place(x=10, y=250)
     entry_prclote = Entry(root, width=30)
-    entry_prclote.place(x=15, y=250)
+    entry_prclote.place(x=15, y=275)
 
     label_prcuni = Label(root, text="Preço Unitário")
-    label_prcuni.place(x=10, y=275)
+    label_prcuni.place(x=10, y=300)
     entry_prcuni = Entry(root, width=30)
-    entry_prcuni.place(x=15, y=300)
+    entry_prcuni.place(x=15, y=325)
 
     label_prcvenda = Label(root, text="Preço de Venda:")
-    label_prcvenda.place(x=10, y=325)
+    label_prcvenda.place(x=10, y=350)
     entry_prcvenda = Entry(root, width=30)
-    entry_prcvenda.place(x=15, y=350)
+    entry_prcvenda.place(x=15, y=375)
 
     butao_confirmar = Button(root, text="CONFIRMAR", command= cadastrar_produto)
-    butao_confirmar.place(x=60, y = 400)
+    butao_confirmar.place(x=60, y = 450)
 
     button_cancel = Button(root, text='CANCELAR', width=10, command=lambda: [clear(), botoes()])
-    button_cancel.place(x=60, y = 450)
+    button_cancel.place(x=60, y = 500)
 
     botao_compra = Button(root, text='Comprar produtos', width=30, command=lambda: [clear(), compra_de_produtos()])
     botao_compra.place(x=830, y=450)
  
-    botaoremover = Button(root, text="Remover produtos", width = 30, command=lambda: [clear(), remocao_de_produtos()])
+    botaoremover = Button(root, text="Remover produtos", width = 30, command=remocao_de_produtos)
     botaoremover.place(x = 600, y = 450)
 
     label_estoque = Label(root, text="ESTOQUE", font="Arial 20")
-    label_estoque.place(x=620, y=10)
+    label_estoque.place(x=585, y=10)
 
-    tree_estoque = ttk.Treeview(root, columns=("Nome do Produto", "Categoria", "Lote","Quantidade", "Preço do Lote", "Preço do Unitário", "Preço de Venda"), show="headings", height=18, )
-    tree_estoque.place(x=325, y=50)
+    tree_estoque = ttk.Treeview(root, columns=("ID","Nome do Produto", "Categoria", "Validade","Quantidade", "Preço do Lote", "Preço do Unitário", "Preço de Venda"), show="headings", height=18 )
+    tree_estoque.place(x=275, y=50)
     
-    for i in ["Nome do Produto", "Categoria", "Lote","Quantidade", "Preço do Lote", "Preço do Unitário", "Preço de Venda"]:
+    for i in ["ID", "Nome do Produto", "Categoria", "Validade","Quantidade", "Preço do Lote", "Preço do Unitário", "Preço de Venda"]:
         tree_estoque.heading(f"{i}", text=f"{i}")
-    
-    tree_estoque.column("Nome do Produto", width=150)
-    tree_estoque.column("Categoria", width=100)
-    tree_estoque.column("Lote", width=100)
-    tree_estoque.column("Quantidade", width=75)
-    tree_estoque.column("Preço do Lote", width=100)
-    tree_estoque.column("Preço do Unitário", width=100)
-    tree_estoque.column("Preço de Venda", width=100)
+
+    tree_estoque.column("ID", width=50, anchor="center")
+    tree_estoque.column("Nome do Produto", width=150, anchor="center")
+    tree_estoque.column("Categoria", width=100, anchor="center")
+    tree_estoque.column("Validade", width=100, anchor="center")
+    tree_estoque.column("Quantidade", width=75, anchor="center")
+    tree_estoque.column("Preço do Lote", width=100, anchor="center")
+    tree_estoque.column("Preço do Unitário", width=100, anchor="center")
+    tree_estoque.column("Preço de Venda", width=100, anchor="center")
 
     scrollbar = Scrollbar(root, orient=VERTICAL, command=tree_estoque.yview)
     scrollbar.place(x=1050, y=49, height=388)
@@ -246,10 +285,9 @@ def tab_estoque():
 def botoes():
     frame_buttonsss = Frame(root)
    
-    butao_estoque = Button(root, text='GERENCIAR ESTOQUE', width=30, height=10, command=lambda: [clear(), tab_estoque()])
+    butao_estoque = Button(root, text='GERENCIAR ESTOQUE', width=30, height=10, command=lambda: [clear(), tab_estoque(), carregar_estoque()])
     butao_estoque.place(x=100, y=220)
    
- 
     butao_produtos = Button(root, text='PRODUTOS COMPRADOS', width=30, height=10, command=clear)
     butao_produtos.place(x=450, y=220)
  
